5cf1833ebd4cd9410ce5da43d75ed7bf89945aed
Namrata65 <62306306+Namrata65@users.noreply.github.com>
2024-12-22 18:11:27 -0800

    logo images

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
-import img from '../resources/nams.jpeg';
+import web from '../resources/techLogo/icons8-web-development-100.png';
+import webDesign from '../resources/techLogo/icons8-web-design-80.png';
+import development from '../resources/techLogo/icons8-web-100.png';
+import software from '../resources/techLogo/icons8-rocket-96.png';
+import design from '../resources/techLogo/icons8-color-swatch-96.png';
 
 const Services = () => {
   return (
@@ -12,22 +16,22 @@ const Services = () => {
             <div className='services-content'>
                 <ul>
                     <li>
-                        <img src={img} alt="Services logo icon" />
+                        <img src={web} alt="Services logo icon" />
                         <h5>Web Development</h5>
                         <p>Crafting dynamic, responsive, and user-friendly websites tailored to your business needs.</p>
                     </li>
                     <li>
-                        <img src={img} alt="Services logo icon" />
+                        <img src={software} alt="Services logo icon" />
                         <h5>Application Development</h5>
                         <p>Building intuitive and scalable mobile and web applications for seamless user experiences.</p>
                     </li>
                     <li>
-                        <img src={img} alt="Services logo icon" />
+                        <img src={design} alt="Services logo icon" />
                         <h5>Web Designing</h5>
                         <p>Designing visually stunning and engaging websites that reflect your brand identity.</p>
                     </li>
                     <li>
-                        <img src={img} alt="Services logo icon" />
+                        <img src={development} alt="Services logo icon" />
                         <h5>Graphic Designing</h5>
                         <p>Creating compelling visuals and graphics to bring your ideas to life with creativity and precision.</p>
                     </li>
